f2d33676bd91daa5b22e3e6878925b0295ee42f5
nilamkhedkar1 <nilam.khedkar1@gmail.com>
2018-10-29 18:27:57 +0530

    Update install_composer.sh

--- scripts/install_composer.sh
+++ scripts/install_composer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /var/www/html/awscodedeploy && git pull 
-cd /var/www/html/awscodedeploy && /usr/local/bin/composer install
+cd /var/www/html/awscodedeploy && /usr/local/bin/composer update symfony/flex --no-plugins
